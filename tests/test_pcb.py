0c935e7253e7906b334dba02ea2ffc43af0ef3b8
Trevor Swan <tcs94@case.edu>
2025-07-08 12:26:07 -0400

    worked on more transfering

--- tests/test_pcb.py
+++ tests/test_pcb.py
@@ -1,5 +1,5 @@
 import rootlab_lib.serial_reader as serial_reader
-from rootlab_lib.voltage_analysis import _plot_analog_pin_data
+import rootlab_lib.voltage_analysis as voltage_analysis
 
 def test_gather():
     out_dir = "../test_files/"
@@ -8,7 +8,9 @@ def test_gather():
     )
     
 def test_plot():
-    _plot_analog_pin_data("../test_data/data5.txt", 2)
+    out_dir = "../test_files/"
+    # _plot_analog_pin_data("../test_data/data5.txt", 2, output_dir=out_dir)
+    voltage_analysis._plot_original_voltage_pos_series("../test_data/data5.txt", plateaus=True, legend=True, output_series_dir=out_dir, output_plats_dir=out_dir)
 
 if __name__ == "__main__":
     # test_gather()
